f1e522f0db5516379f268b5052c358122e8150c8
Fco-Gabriel-Silva <fco.gabrielsilva2408@gmail.com>
2024-10-25 15:30:46 -0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 import AddTasks from "./components/AddTasks";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 } from "uuid";
+import Title from "./components/Title";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar Programação",
-      description:
-        "Estudar programação para se tornar um desenvolvedor full stack.",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Ler Livros",
-      description: "Ler 20 livros por semana.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar Inglês",
-      description: "Estudar inglês para se tornar fluente em inglês.",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // useEffect(() => {
+  //   async function fetchTasks() {
+  //     // CHAMAR A API
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/todos?_limit=10",
+  //       {
+  //         method: "GET",
+  //       }
+  //     );
+  // PEGAR OS DADOS QUE ELA RETORNA
+  //const data = await response.json();
+
+  // ARMAZENAR/PERSISTIR ESSES DADOS NO STATE
+
+  // Se quiser, pode pegar os dados de uma api:
+  //setTasks(data);
+  //}
+  //fetchTasks();
+  // }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -57,9 +64,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          Gerenciador de Tarefas
-        </h1>
+        <Title>Gerenciador de Tarefas</Title>
         <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
